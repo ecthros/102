dce5064161138c58491d1f1fc8ec7832f80142a0
George Hughey <ghughey@umd.edu>
2017-02-01 19:31:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-OS=$(uname)
-if [ "$OS" == "Linux" ]; then
-	sudo apt-get install python-pip python-dev libssl-dev libffi-dev curl
-fi
+sudo apt-get install python-pip python-dev libssl-dev libffi-dev curl
 
 sudo -H pip install --upgrade pyopenssl
 sudo -H pip install --upgrade oauth2client
