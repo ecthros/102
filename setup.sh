e113eeb67aba8d295c477692b8de077fbb1332db
hp <hp@aceslab.umd.edu>
2017-02-20 13:13:37 -0500

    Dependencies

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt-get install python3-pip python3-dev libssl-dev libffi-dev curl
 
-sudo -H pip3 install setuptools=33.1.1
+sudo -H pip3 install setuptools==33.1.1
 sudo -H pip3 install pyopenssl
 sudo -H pip3 install --upgrade oauth2client
 sudo -H pip3 install --upgrade gspread
